b1fc7b6f0b8049e361e7b604dbfc33cbfc481e1e
Toua Thao <touathao09@gmail.com>
2018-06-26 14:31:27 -0500

    fix the wording

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ let express = require('express')
 let app = express();
 const PORT = 5000;
 
-app.use(express.static('server/public'))
+app.use(express.static('Server/public'))
 
 app.listen(PORT,function(){
     console.log('In')
